7331cc2b8c6a2d0b883bda3e815d08105cb544d3
Nick-qsv <w0m85k27@protonmail.com>
2024-03-04 14:57:50 -0500

    Initial Commit

---
+++ DiceV1/DiceV1App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceV1App.swift
+//  DiceV1
+//
+//  Created by Nicolas Baez on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
