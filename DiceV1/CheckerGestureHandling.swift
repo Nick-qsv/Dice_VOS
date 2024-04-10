acb0f0db1656160bbe401a07d83f72130ab689cf
Nick-qsv <w0m85k27@protonmail.com>
2024-04-10 12:20:34 -0400

    Added Tap Gesture differentiating based on component. Next step is integrating the Tap Gesture logic

---
+++ DiceV1/CheckerGestureHandling.swift
@@ -0,0 +1,18 @@
+//
+//  CheckerGestureHandling.swift
+//  DiceV1
+//
+//  Created by Nicolas Baez on 4/9/24.
+//
+
+import SwiftUI
+
+struct CheckerGestureHandling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckerGestureHandling()
+}
